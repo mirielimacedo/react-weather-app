1c30eb7dbbaa0a0958ba4e287a9678083a8d4eac
mirielimacedo <mirielimacedo@gmail.com>
2024-04-10 12:42:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
 
-export default App;
+      <footer>
+        This project was coded by {""}
+        <a href="https://www.linkedin.com/feed/" target="_blank">
+          Mirieli Maria{" "}
+        </a>{" "}
+        an is {""}
+        <a
+          href="https://github.com/mirielimacedo/react-weather-app"
+          target="_blank"
+        >
+          open-source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
