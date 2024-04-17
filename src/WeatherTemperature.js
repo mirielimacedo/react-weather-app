8ed325e6f9fe64e8671087b2c323ae62e050ad5c
mirielimacedo <mirielimacedo@gmail.com>
2024-04-17 14:31:29 +0800

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -7,10 +7,6 @@ export default function WeatherTemperature(props) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
 
   function fahrenheit() {
     return (props.celsius * 9) / 5 + 32;
