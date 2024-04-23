47e4eb33c86b426e9c617270df2440808f035872
mirielimacedo <mirielimacedo@gmail.com>
2024-04-23 12:21:38 +0800

    Add API code in WeatherForescat

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather({ city, onCityChange }) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
@@ -64,7 +65,7 @@ export default function Weather({ city, onCityChange }) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
