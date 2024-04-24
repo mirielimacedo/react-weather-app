7a7035d200ec51bd9796252e0cb102ab9548382d
mirielimacedo <mirielimacedo@gmail.com>
2024-04-24 13:49:56 +0800

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,11 +4,17 @@ import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
 
   useEffect(() => {
     setLoaded(false);
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const longitude = props.coordinates.lon;
+    const latitude = props.coordinates.lat;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
   }, [props.coordinates]);
 
   function handleResponse(response) {
@@ -28,18 +34,13 @@ export default function WeatherForecast(props) {
                 </div>
               );
             }
+            // Adicione o retorno null aqui caso a condição do if não seja atendida
+            return null;
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
     return null;
   }
 }
